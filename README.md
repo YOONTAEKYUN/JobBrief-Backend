0869e384af810ceeaa093cb096defac390ea2af6
tk.yoon <tk.yoon0105@gmail.com>
2023-05-10 13:00:26 +0900

    spring boot 프로젝트 생성 Vue, MySQL 연동

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JobBrief
+23-1학기 capstone 팀 프로젝트
+JobBrief, (명지대학교 인문캠퍼스) 취업 준비생을 위한 직무별 뉴스 요약 서비스
+
+sparing boot 
+-version: 3.05
+-Gradle - Groovy
+-Java 17
+-Packaging: Jar
---
+++ src/main/java/co/kr/capstonemju/JobBrief/JobBriefApplication.java
@@ -0,0 +1,13 @@
+package co.kr.capstonemju.JobBrief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBriefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBriefApplication.class, args);
+	}
+
+}
