f6c8ccebff217645cb6263338263f0d6862f318a
tk.yoon <tk.yoon0105@gmail.com>
2023-05-26 23:46:36 +0900

    refactor: 요구사항 변경에 따른 이메일 중복 에러 코드 삭제

--- src/main/java/co/kr/capstonemju/JobBrief/global/exception/ErrorCode.java
+++ src/main/java/co/kr/capstonemju/JobBrief/global/exception/ErrorCode.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     USERID_DUPLICATED(HttpStatus.CONFLICT,"사용자 아이디 DUPLICATE "),
-    USEREMAIL_DUPLICATED(HttpStatus.CONFLICT,"사용자 이메일 DUPLICATE "),
     USERID_NOT_FOUND(HttpStatus.NOT_FOUND,"사용자 아이디 NOT FOUND "),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"사용자 비밀번호 INVALID");
     private HttpStatus httpStatus;
