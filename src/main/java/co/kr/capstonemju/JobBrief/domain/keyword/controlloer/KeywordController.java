fd4c119e47b962d10dc6ee76c44428fe7894c4d5
tk.yoon <tk.yoon0105@gmail.com>
2023-06-10 18:07:02 +0900

    feat: 키워드 검색시에도 목록에 북마크 여부 값 dto에 추가

--- src/main/java/co/kr/capstonemju/JobBrief/domain/keyword/controlloer/KeywordController.java
+++ src/main/java/co/kr/capstonemju/JobBrief/domain/keyword/controlloer/KeywordController.java
@@ -1,7 +1,9 @@
 package co.kr.capstonemju.JobBrief.domain.keyword.controlloer;
 
 import co.kr.capstonemju.JobBrief.domain.keyword.service.KeywordService;
-import co.kr.capstonemju.JobBrief.domain.news.controller.dto.NewsListDto;
+import co.kr.capstonemju.JobBrief.domain.member.model.Member;
+import co.kr.capstonemju.JobBrief.domain.news.controller.dto.NewsListForMemberDto;
+import co.kr.capstonemju.JobBrief.global.CurrentUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,8 +14,8 @@ public class KeywordController {
     private final KeywordService keywordService;
 
     @GetMapping("/search")
-    public NewsListDto searchNewsListByKeyword(@RequestParam String keyword, @RequestParam(defaultValue = "1") int page){
-        return keywordService.searchNewsListByKeyword(keyword, page);
+    public NewsListForMemberDto searchNewsListByKeyword(@CurrentUser Member member, @RequestParam String keyword, @RequestParam(defaultValue = "1") int page){
+        return keywordService.searchNewsListByKeyword(member, keyword, page);
     }
 
 }
--- src/main/java/co/kr/capstonemju/JobBrief/domain/keyword/service/KeywordService.java
+++ src/main/java/co/kr/capstonemju/JobBrief/domain/keyword/service/KeywordService.java
@@ -1,10 +1,16 @@
 package co.kr.capstonemju.JobBrief.domain.keyword.service;
 
+import co.kr.capstonemju.JobBrief.domain.bookmark.model.Bookmark;
+import co.kr.capstonemju.JobBrief.domain.bookmark.repository.BookmarkRepository;
+import co.kr.capstonemju.JobBrief.domain.member.model.Member;
 import co.kr.capstonemju.JobBrief.domain.news.controller.dto.NewsDto;
+import co.kr.capstonemju.JobBrief.domain.news.controller.dto.NewsForMemberDto;
 import co.kr.capstonemju.JobBrief.domain.news.controller.dto.NewsListDto;
+import co.kr.capstonemju.JobBrief.domain.news.controller.dto.NewsListForMemberDto;
 import co.kr.capstonemju.JobBrief.domain.news.model.News;
 import co.kr.capstonemju.JobBrief.domain.news.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,16 +23,41 @@ import java.util.stream.Collectors;
 @Transactional
 public class KeywordService {
     private final NewsRepository newsRepository;
+    private final BookmarkRepository bookmarkRepository;
     private static final int PAGE_SIZE = 10;
 
-    public NewsListDto searchNewsListByKeyword(String keyword, int page) {
+    public NewsListForMemberDto searchNewsListByKeyword(Member member, String keyword, int page) {
         PageRequest pageRequest = PageRequest.of(page - 1, PAGE_SIZE);
-        List<News> newsList = newsRepository.findByKeywordsKeywordNameContaining(keyword, pageRequest);
-        List<NewsDto> newsDtoList = newsList.stream()
-                .map(NewsDto::new)
-                .collect(Collectors.toList());
-        long totalItems = newsRepository.countByKeywordsKeywordNameContaining(keyword);
-        int totalPages = (int) Math.ceil((double) totalItems / PAGE_SIZE);
-        return new NewsListDto(newsDtoList, page, totalPages);
+
+        if (member != null) {
+            // 회원인 경우
+            List<News> newsList = newsRepository.findByKeywordsKeywordNameContaining(keyword, pageRequest);
+
+
+            List<NewsForMemberDto> newsForMemberDtoList = newsList.stream()
+                    .map(news -> {
+                        // 각 뉴스마다 북마크 상태를 확인하여 설정합니다.
+                        boolean isBookmarked = bookmarkRepository.findByNewsAndMember(news, member) != null;
+                        return new NewsForMemberDto(news, isBookmarked);
+                    })
+                    .collect(Collectors.toList());
+
+            long totalItems = newsRepository.countByKeywordsKeywordNameContaining(keyword);
+            int totalPages = (int) Math.ceil((double) totalItems / PAGE_SIZE);
+            return new NewsListForMemberDto(newsForMemberDtoList, page, totalPages);
+        }
+        else {
+            // 비회원인 경우
+            List<News> newsList = newsRepository.findByKeywordsKeywordNameContaining(keyword, pageRequest);
+            List<NewsForMemberDto> newsForMemberDtoList = newsList.stream()
+                    .map(news -> new NewsForMemberDto(news, false))
+                    .collect(Collectors.toList());
+
+            long totalItems = newsRepository.countByKeywordsKeywordNameContaining(keyword);
+            int totalPages = (int) Math.ceil((double) totalItems / PAGE_SIZE);
+            return new NewsListForMemberDto(newsForMemberDtoList, page, totalPages);
+        }
+
+
     }
 }
